f89ea1210254e968e91cdc3331d6f512023bedb5
Matt Belk <matthew.belk96@gmail.com>
2019-09-16 14:48:33 +0100

    Solve problem 7

--- Program.cs
+++ Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace _007
 {
     class Program
     {
+        //This problem is solved by simply finding primes until we have found 10001 of them.
+        //Primes are added to a list, which is then used to check prime divisors of each number.
         public static int Solution()
         {
-            
+            const int limit = 10001;
+            int counter = 1;
+            bool isPrime = true;
+            int j = 0;
+            List<int> primes = new List<int>();
+
+            primes.Add(2);
+
+            while (primes.Count < limit)
+            {
+                counter += 2;
+                j = 0;
+                isPrime = true;
+                while (primes[j] * primes[j] <= counter)
+                {
+                    if (counter % primes[j] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (isPrime)
+                {
+                    primes.Add(counter);
+                }
+            }
+            return primes[limit - 1];
         }
 
         static void Main(string[] args)
