7f49091a71de63d5ceb636afc8f418630847edfa
Matt Belk <matthew.belk96@gmail.com>
2019-09-16 15:56:05 +0100

    Solve problem 9

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace _009
     {
         public static int Solution()
         {
-            
+            //Although we could use number theory to find a more elegant solution, a brute force solution shouldn't be too bad for performance.
+            //We can reduce the number of checks needed by noting that, since a < b < c, we have a < sum/3 and b < sum/2.
+            const int sum = 1000;
+            int a = 0, b = 0, c = 0;
+            bool found = false;
+
+            for(a = 1; a < sum / 3; a++)
+            {
+                for(b = 1; b < sum / 2; b++)
+                {
+                    //Since a + b + c = sum
+                    c = sum - a - b;
+
+                    if (a * a + b * b == c * c)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            return a * b * c;
         }
 
         static void Main(string[] args)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Special Pythagorean triplet
+# Special Pythagorean triplet (solution in less than 1 ms)
 ## [Problem 9](https://projecteuler.net/problem=9)
 
 > A Pythagorean triplet is a set of three natural numbers, *a < b < c*, for which,
@@ -7,4 +7,4 @@
 > For example, 3<sup>2</sup> + 4<sup>2</sup> = 9 + 16 = 25 = 5<sup>2</sup>.
 
 > There exists exactly one Pythagorean triplet for which *a + b + c* = 1000.
-> Find the product abc.
+> Find the product *abc*.
