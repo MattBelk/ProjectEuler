dd665c3ced19b48528f450ff9d6f9df2c069d96e
Matt Belk <matthew.belk96@gmail.com>
2019-09-16 14:32:37 +0100

    Solve problem 6

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@ namespace _006
 {
     class Program
     {
-        public static int Solution()
+        //Rather than calculate each expression and take the difference, note that
+        //the sum of the first n natural numbers is n*(n+1)/2, and that the sum of the 
+        //squares of the first n natural numbers is n*(n+1)*(2n+1)/6.
+        //This gives us a very simple way to solve the problem, rather than brute force.
+
+        public static long Solution()
         {
-            
+            const long n = 100;
+            long sum = (n * (n + 1)) / 2;
+            long squares = (n * (n + 1) * (2 * n + 1)) / 6;
+            return sum * sum - squares;
         }
 
         static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            int result = Solution();
+            long result = Solution();
             stopwatch.Stop();
             long time = stopwatch.ElapsedMilliseconds;
 
