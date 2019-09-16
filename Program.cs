f489c793c9aac28af0fd43dfbb785d6121742685
Matt Belk <matthew.belk96@gmail.com>
2019-09-16 14:13:54 +0100

    Set up project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _006
+{
+    class Program
+    {
+        public static int Solution()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = Solution();
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Result: {result} \t Time: {time} milliseconds");
+            Console.WriteLine("Press any key to quit!");
+            Console.ReadKey();
+        }
+    }
+}
